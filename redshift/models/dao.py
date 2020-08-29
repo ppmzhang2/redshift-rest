752d5ff740649a269c91ccaaa3a1584432b4d667
Zhang <ztz2000@gmail.com>
2020-08-29 22:44:25 +0800

    [new] 'load_sample' method

--- redshift/models/dao.py
+++ redshift/models/dao.py
@@ -1,3 +1,4 @@
+import re
 from functools import wraps
 from typing import List, NoReturn
 
@@ -73,6 +74,42 @@ class Dao(metaclass=SingletonMeta):
 
     def all_tables(self) -> List[str]:
         return self._engine.table_names()
+
+    def load_sample(self) -> NoReturn:
+        def statement(table: str, filename: str, delimiter: str,
+                      timeformat: str) -> str:
+            return f'''copy {table} from
+                's3://{Config.S3_BUCKET}/tickit/{filename}'
+                credentials 'aws_iam_role={Config.RS_IAM_ROLE}'
+                delimiter '{delimiter}' {timeformat}
+                region '{Config.S3_REGION}';
+                '''
+
+        dlm_map = {'pipe': '|', 'tab': '\\t'}
+        tables = [
+            'users', 'venue', 'category', 'date', 'event', 'listing', 'sales'
+        ]
+        files = [
+            'allusers_pipe.txt',
+            'venue_pipe.txt',
+            'category_pipe.txt',
+            'date2008_pipe.txt',
+            'allevents_pipe.txt',
+            'listings_pipe.txt',
+            'sales_tab.txt',
+        ]
+        delimiters = [
+            dlm_map[next(
+                filter(lambda i: i in ['pipe', 'tab'], re.split(r'[_.]', s)))]
+            for s in files
+        ]
+        formats = [
+            '', '', '', '', "timeformat 'YYYY-MM-DD HH:MI:SS'", '',
+            "timeformat 'MM/DD/YYYY HH:MI:SS'"
+        ]
+        with self._engine.begin() as conn:
+            for tb, file, dlm, fmt in zip(tables, files, delimiters, formats):
+                conn.execute(statement(tb, file, dlm, fmt))
 
     def all_users(self) -> List[Users]:
         @_commit
