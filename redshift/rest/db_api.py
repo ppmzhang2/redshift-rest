c67cc21d0884a67a52d69fcd50d3c1f02a13075e
Zhang <ztz2000@gmail.com>
2020-08-30 21:52:00 +0800

    [fix] remove concurrency to walk-around 'attached to a different loop' issue

--- redshift/rest/db_api.py
+++ redshift/rest/db_api.py
@@ -1,6 +1,3 @@
-from concurrent.futures.thread import ThreadPoolExecutor
-
-import asyncio
 from aiohttp import web
 from aiohttp.abc import Request
 
@@ -8,20 +5,12 @@ from redshift.models.dao import Dao
 
 
 class DbApi(object):
-    __slots__ = ['_dao', '_executor', '_loop']
+    __slots__ = ['_dao']
 
     def __init__(self):
         self._dao = Dao()
-        self._executor = ThreadPoolExecutor(10)
-        self._loop = asyncio.get_event_loop()
 
     async def json_total_sales(self, request: Request):
-        async def _aio_total_sales(dt: str):
-            def helper():
-                return self._dao.total_sales(dt=dt)
-
-            return await self._loop.run_in_executor(self._executor, helper)
-
         date = request.rel_url.query.get('date')
-        sales = await _aio_total_sales(date)
+        sales = self._dao.total_sales(dt=date)
         return web.json_response(data={'sales': sales})
