62ade0375b30fa4c51efd5e4adace97d1db06478
Zhang <ztz2000@gmail.com>
2020-08-31 15:33:59 +0800

    [new] logging dict config

--- config.py
+++ config.py
@@ -15,3 +15,58 @@ class Config(object):
     RS_IAM_ROLE = ''
     S3_BUCKET = ''
     S3_REGION = ''
+    # Logging
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'nano': {
+                'format': '%(asctime)s  %(message)s'
+            },
+            'micro': {
+                'format':
+                '%(asctime)s [%(levelname)s] '
+                '%(name)s: '
+                '%(message)s',
+            },
+            'small': {
+                'format':
+                '%(asctime)s [%(levelname)s] '
+                '%(name)s - %(module)s - %(filename)s - %(lineno)s: '
+                '%(message)s',
+            },
+        },
+        'handlers': {
+            'info_file': {
+                'level': 'INFO',
+                'formatter': 'micro',
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'when': 'H',
+                'interval': 1,
+                'backupCount': 6,
+                'filename': f'{basedir}/logs/info.log',
+            },
+            'debug_streamer': {
+                'level': 'DEBUG',
+                'formatter': 'small',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
+            },
+            'debug_file': {
+                'level': 'DEBUG',
+                'formatter': 'small',
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'when': 'M',
+                'interval': 1,
+                'backupCount': 30,
+                'filename': f'{basedir}/logs/debug.log',
+            },
+        },
+        'loggers': {
+            'info_logger': {
+                'handlers': ['debug_streamer', 'info_file'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+        },
+    }
