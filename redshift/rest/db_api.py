29f35b31874272235e6e65abaf6b11c57a38bebd
Zhang <ztz2000@gmail.com>
2020-09-01 14:58:30 +0800

    [new] logging decorator for DbApi REST coroutines

--- redshift/rest/db_api.py
+++ redshift/rest/db_api.py
@@ -4,12 +4,12 @@ from aiohttp import web
 from aiohttp.abc import Request
 
 from redshift import cfg
-from redshift.log_maker import LogMaker
+from redshift.log_maker import LogDbApi
 from redshift.models.dao import Dao
 
 logging.config.dictConfig(cfg.LOGGING)
 logger = logging.getLogger('info_logger')
-log_maker = LogMaker(logger)
+log_maker = LogDbApi(logger)
 
 
 class DbApi(object):
