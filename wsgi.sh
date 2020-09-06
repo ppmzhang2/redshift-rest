8e3873dd5396abde0b311cbd1ba492343460bbb2
Zhang <ztz2000@gmail.com>
2020-09-06 20:58:38 +0800

    [fix] dockerized environment variables

--- wsgi.sh
+++ wsgi.sh
@@ -3,7 +3,7 @@
 CPUS=$(getconf _NPROCESSORS_ONLN 2> /dev/null)
 LOG_FILE="/app/logs/gunicorn.log"
 WORKERS=$((2*CPUS+1))
-BIND=127.0.0.1:8000
+BIND=0.0.0.0:8000
 APP_MODULE=run:app
 
 exec gunicorn $APP_MODULE --bind=$BIND \
